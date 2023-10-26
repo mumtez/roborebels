79c9e1551139233d5cbe27a47313018ac757bdf0
Sri r. a. S <133979357+Makadefu@users.noreply.github.com>
2023-10-26 17:23:45 -0400

    added intake motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode.java
@@ -12,17 +12,44 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @TeleOp(name = "Basic Teleop", group = "Teleop")
 public class BasicOpMode extends LinearOpMode {
 
+  private DcMotor intake;
+  private DcMotor frontLeftMotor;
+  private DcMotor backLeftMotor;
+  private DcMotor frontRightMotor;
+  private DcMotor backRightMotor;
+
+
+  private void initialize(){
+    // Initialize components
+    this.intake = hardwareMap.dcMotor.get("intake");
+    this.frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+    this.backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+    this.frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+    this.backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+    // Set modes
+    this.intake.setMode(RunMode.RUN_WITHOUT_ENCODER);
+    this.frontLeftMotor.setMode(RunMode.RUN_WITHOUT_ENCODER);
+    this.backLeftMotor.setMode(RunMode.RUN_WITHOUT_ENCODER);
+    this.frontRightMotor.setMode(RunMode.RUN_WITHOUT_ENCODER);
+    this.backRightMotor.setMode(RunMode.RUN_WITHOUT_ENCODER);
+
+    this.frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    this.backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    this.frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    this.backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+  }
+
   @Override
   public void runOpMode() throws InterruptedException {
     int toGo = 0;
 
     DcMotor intake = hardwareMap.dcMotor.get("intake");
-
     DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
     DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
     DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
     DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-
 
     intake.setMode(RunMode.RUN_WITHOUT_ENCODER);
 
@@ -36,6 +63,7 @@ public class BasicOpMode extends LinearOpMode {
     frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+    // Starting here you would also add this as a private field and change the references to .this
     DcMotor slideMotor = hardwareMap.dcMotor.get("slideMotor");
 
     slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -66,8 +94,8 @@ public class BasicOpMode extends LinearOpMode {
       double x = gamepad1.left_stick_x;
       double rx = gamepad1.right_stick_x;
 
-      double slide = gamepad1.right_trigger;
-      double unSlide = -gamepad1.left_trigger;
+//      double slide = gamepad1.right_trigger;
+//      double unSlide = -gamepad1.left_trigger;
 
       if (gamepad1.options) {
         imu.resetYaw();
@@ -96,7 +124,6 @@ public class BasicOpMode extends LinearOpMode {
       backRightMotor.setPower(backRightPower);
 
       //slideMotor.setPower(slide + unSlide);
-
 
       if (gamepad1.a) {
         toGo = -5;
