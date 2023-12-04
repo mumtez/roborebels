2543dd03e7a893a419036acb86b511e78fc338e8
Andrew <papertowelman6@gmail.com>
2023-12-04 18:57:02 -0500

    fix

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odom/Localizer.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.odom;
+
+import com.acmerobotics.roadrunner.Time;
+import com.acmerobotics.roadrunner.Twist2dDual;
+
+public interface Localizer {
+
+  Twist2dDual<Time> update();
+}
