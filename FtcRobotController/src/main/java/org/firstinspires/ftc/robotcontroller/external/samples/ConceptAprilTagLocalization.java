599464e373b0ab94be2972d3f2db49ee8a2cf95f
Burtorustum <burtorustum@gmail.com>
2024-11-09 17:33:33 -0500

    SDK v10.1.1

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptAprilTagLocalization.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptAprilTagLocalization.java
@@ -66,180 +66,178 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 @Disabled
 public class ConceptAprilTagLocalization extends LinearOpMode {
 
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+  private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
 
-    /**
-     * Variables to store the position and orientation of the camera on the robot. Setting these
-     * values requires a definition of the axes of the camera and robot:
-     *
-     * Camera axes:
-     * Origin location: Center of the lens
-     * Axes orientation: +x right, +y down, +z forward (from camera's perspective)
-     *
-     * Robot axes (this is typical, but you can define this however you want):
-     * Origin location: Center of the robot at field height
-     * Axes orientation: +x right, +y forward, +z upward
-     *
-     * Position:
-     * If all values are zero (no translation), that implies the camera is at the center of the
-     * robot. Suppose your camera is positioned 5 inches to the left, 7 inches forward, and 12
-     * inches above the ground - you would need to set the position to (-5, 7, 12).
-     *
-     * Orientation:
-     * If all values are zero (no rotation), that implies the camera is pointing straight up. In
-     * most cases, you'll need to set the pitch to -90 degrees (rotation about the x-axis), meaning
-     * the camera is horizontal. Use a yaw of 0 if the camera is pointing forwards, +90 degrees if
-     * it's pointing straight left, -90 degrees for straight right, etc. You can also set the roll
-     * to +/-90 degrees if it's vertical, or 180 degrees if it's upside-down.
-     */
-    private Position cameraPosition = new Position(DistanceUnit.INCH,
-            0, 0, 0, 0);
-    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
-            0, -90, 0, 0);
+  /**
+   * Variables to store the position and orientation of the camera on the robot. Setting these
+   * values requires a definition of the axes of the camera and robot:
+   * <p>
+   * Camera axes: Origin location: Center of the lens Axes orientation: +x right, +y down, +z
+   * forward (from camera's perspective)
+   * <p>
+   * Robot axes (this is typical, but you can define this however you want): Origin location: Center
+   * of the robot at field height Axes orientation: +x right, +y forward, +z upward
+   * <p>
+   * Position: If all values are zero (no translation), that implies the camera is at the center of
+   * the robot. Suppose your camera is positioned 5 inches to the left, 7 inches forward, and 12
+   * inches above the ground - you would need to set the position to (-5, 7, 12).
+   * <p>
+   * Orientation: If all values are zero (no rotation), that implies the camera is pointing straight
+   * up. In most cases, you'll need to set the pitch to -90 degrees (rotation about the x-axis),
+   * meaning the camera is horizontal. Use a yaw of 0 if the camera is pointing forwards, +90
+   * degrees if it's pointing straight left, -90 degrees for straight right, etc. You can also set
+   * the roll to +/-90 degrees if it's vertical, or 180 degrees if it's upside-down.
+   */
+  private Position cameraPosition = new Position(DistanceUnit.INCH,
+      0, 0, 0, 0);
+  private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+      0, -90, 0, 0);
 
-    /**
-     * The variable to store our instance of the AprilTag processor.
-     */
-    private AprilTagProcessor aprilTag;
+  /**
+   * The variable to store our instance of the AprilTag processor.
+   */
+  private AprilTagProcessor aprilTag;
 
-    /**
-     * The variable to store our instance of the vision portal.
-     */
-    private VisionPortal visionPortal;
+  /**
+   * The variable to store our instance of the vision portal.
+   */
+  private VisionPortal visionPortal;
 
-    @Override
-    public void runOpMode() {
+  @Override
+  public void runOpMode() {
 
-        initAprilTag();
+    initAprilTag();
 
-        // Wait for the DS start button to be touched.
-        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
-        telemetry.addData(">", "Touch START to start OpMode");
-        telemetry.update();
-        waitForStart();
+    // Wait for the DS start button to be touched.
+    telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+    telemetry.addData(">", "Touch START to start OpMode");
+    telemetry.update();
+    waitForStart();
 
-        while (opModeIsActive()) {
+    while (opModeIsActive()) {
 
-            telemetryAprilTag();
+      telemetryAprilTag();
 
-            // Push telemetry to the Driver Station.
-            telemetry.update();
+      // Push telemetry to the Driver Station.
+      telemetry.update();
 
-            // Save CPU resources; can resume streaming when needed.
-            if (gamepad1.dpad_down) {
-                visionPortal.stopStreaming();
-            } else if (gamepad1.dpad_up) {
-                visionPortal.resumeStreaming();
-            }
+      // Save CPU resources; can resume streaming when needed.
+      if (gamepad1.dpad_down) {
+        visionPortal.stopStreaming();
+      } else if (gamepad1.dpad_up) {
+        visionPortal.resumeStreaming();
+      }
 
-            // Share the CPU.
-            sleep(20);
-        }
+      // Share the CPU.
+      sleep(20);
+    }
 
-        // Save more CPU resources when camera is no longer needed.
-        visionPortal.close();
+    // Save more CPU resources when camera is no longer needed.
+    visionPortal.close();
 
-    }   // end method runOpMode()
+  }   // end method runOpMode()
 
-    /**
-     * Initialize the AprilTag processor.
-     */
-    private void initAprilTag() {
+  /**
+   * Initialize the AprilTag processor.
+   */
+  private void initAprilTag() {
 
-        // Create the AprilTag processor.
-        aprilTag = new AprilTagProcessor.Builder()
+    // Create the AprilTag processor.
+    aprilTag = new AprilTagProcessor.Builder()
 
-                // The following default settings are available to un-comment and edit as needed.
-                //.setDrawAxes(false)
-                //.setDrawCubeProjection(false)
-                //.setDrawTagOutline(true)
-                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-                //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-                .setCameraPose(cameraPosition, cameraOrientation)
+        // The following default settings are available to un-comment and edit as needed.
+        //.setDrawAxes(false)
+        //.setDrawCubeProjection(false)
+        //.setDrawTagOutline(true)
+        //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+        //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+        //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+        .setCameraPose(cameraPosition, cameraOrientation)
 
-                // == CAMERA CALIBRATION ==
-                // If you do not manually specify calibration parameters, the SDK will attempt
-                // to load a predefined calibration for your camera.
-                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
-                // ... these parameters are fx, fy, cx, cy.
+        // == CAMERA CALIBRATION ==
+        // If you do not manually specify calibration parameters, the SDK will attempt
+        // to load a predefined calibration for your camera.
+        //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
+        // ... these parameters are fx, fy, cx, cy.
 
-                .build();
+        .build();
 
-        // Adjust Image Decimation to trade-off detection-range for detection-rate.
-        // eg: Some typical detection data using a Logitech C920 WebCam
-        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
-        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
-        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
-        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
-        // Note: Decimation can be changed on-the-fly to adapt during a match.
-        //aprilTag.setDecimation(3);
+    // Adjust Image Decimation to trade-off detection-range for detection-rate.
+    // eg: Some typical detection data using a Logitech C920 WebCam
+    // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+    // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+    // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
+    // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
+    // Note: Decimation can be changed on-the-fly to adapt during a match.
+    //aprilTag.setDecimation(3);
 
-        // Create the vision portal by using a builder.
-        VisionPortal.Builder builder = new VisionPortal.Builder();
+    // Create the vision portal by using a builder.
+    VisionPortal.Builder builder = new VisionPortal.Builder();
 
-        // Set the camera (webcam vs. built-in RC phone camera).
-        if (USE_WEBCAM) {
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        } else {
-            builder.setCamera(BuiltinCameraDirection.BACK);
-        }
+    // Set the camera (webcam vs. built-in RC phone camera).
+    if (USE_WEBCAM) {
+      builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+    } else {
+      builder.setCamera(BuiltinCameraDirection.BACK);
+    }
 
-        // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
+    // Choose a camera resolution. Not all cameras support all resolutions.
+    //builder.setCameraResolution(new Size(640, 480));
 
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableLiveView(true);
+    // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+    //builder.enableLiveView(true);
 
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+    // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+    //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
 
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
+    // Choose whether or not LiveView stops if no processors are enabled.
+    // If set "true", monitor shows solid orange screen if no processors enabled.
+    // If set "false", monitor shows camera view without annotations.
+    //builder.setAutoStopLiveView(false);
 
-        // Set and enable the processor.
-        builder.addProcessor(aprilTag);
+    // Set and enable the processor.
+    builder.addProcessor(aprilTag);
 
-        // Build the Vision Portal, using the above settings.
-        visionPortal = builder.build();
+    // Build the Vision Portal, using the above settings.
+    visionPortal = builder.build();
 
-        // Disable or re-enable the aprilTag processor at any time.
-        //visionPortal.setProcessorEnabled(aprilTag, true);
+    // Disable or re-enable the aprilTag processor at any time.
+    //visionPortal.setProcessorEnabled(aprilTag, true);
 
-    }   // end method initAprilTag()
+  }   // end method initAprilTag()
 
-    /**
-     * Add telemetry about AprilTag detections.
-     */
-    private void telemetryAprilTag() {
+  /**
+   * Add telemetry about AprilTag detections.
+   */
+  private void telemetryAprilTag() {
 
-        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-        telemetry.addData("# AprilTags Detected", currentDetections.size());
+    List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+    telemetry.addData("# AprilTags Detected", currentDetections.size());
 
-        // Step through the list of detections and display info for each one.
-        for (AprilTagDetection detection : currentDetections) {
-            if (detection.metadata != null) {
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
-                        detection.robotPose.getPosition().x,
-                        detection.robotPose.getPosition().y,
-                        detection.robotPose.getPosition().z));
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
-                        detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
-                        detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
-                        detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
-            } else {
-                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
-            }
-        }   // end for() loop
+    // Step through the list of detections and display info for each one.
+    for (AprilTagDetection detection : currentDetections) {
+      if (detection.metadata != null) {
+        telemetry.addLine(
+            String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+        telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
+            detection.robotPose.getPosition().x,
+            detection.robotPose.getPosition().y,
+            detection.robotPose.getPosition().z));
+        telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
+            detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
+            detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
+            detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES)));
+      } else {
+        telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+        telemetry.addLine(
+            String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+      }
+    }   // end for() loop
 
-        // Add "key" information to telemetry
-        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+    // Add "key" information to telemetry
+    telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+    telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
 
-    }   // end method telemetryAprilTag()
+  }   // end method telemetryAprilTag()
 
 }   // end class
