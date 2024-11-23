84d9756293f3dc82b00e2abd3fdde49661ec9cbb
andrewaburustum <aaburustum26@saddleriverday.org>
2024-11-22 23:24:17 -0500

    good

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,71 +3,71 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @Config
 @TeleOp(name = "Teleop")
 public class Teleop extends LinearOpMode {
+    Robot robot;
 
-  Robot robot;
+    boolean slideOut = false;
+    boolean outtaking = false;
 
-  boolean slideOut = false;
-  boolean outtaking = false;
+    int timer = 60;
+    int timer2 = 60;
 
-  int timer = 60;
-  int timer2 = 60;
+    boolean timerDone = false;
+    boolean timerDone2 = false;
 
-  boolean timerDone = false;
-  boolean timerDone2 = false;
+    double horizontalPos = 0;
 
-  double horizontalPos = 0;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(this);
 
-  @Override
-  public void runOpMode() throws InterruptedException {
-    robot = new Robot(this);
+        waitForStart();
+        // START
 
-    waitForStart();
-    // START
+        // LOOP
+        while (opModeIsActive()) {
+            int hangPos = robot.hang.getCurrentPosition();
+            int vSlideLPos = robot.slideLeft.getCurrentPosition();
+            int vSlideRPos = robot.slideRight.getCurrentPosition();
 
-    // LOOP
-    while (opModeIsActive()) {
-      int hangPos = robot.hang.getCurrentPosition();
-      int vSlideLPos = robot.slideLeft.getCurrentPosition();
-      int vSlideRPos = robot.slideRight.getCurrentPosition();
+            if (gamepad1.left_bumper) {
+                robot.drive.lazyImu.get().resetYaw();
+            }
 
-      if (gamepad1.left_bumper) {
-        robot.drive.lazyImu.get().resetYaw();
-      }
+            // === FIELD CENTRIC ===
 
-      // === FIELD CENTRIC ===
+            boolean slideGoOut;
 
-      boolean slideGoOut;
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
-      double y = -gamepad1.left_stick_y;
-      double x = gamepad1.left_stick_x;
-      double rx = gamepad1.right_stick_x;
+            double botHeading = AngleUnit.DEGREES.toRadians(robot.getHeading());
 
-      double botHeading = AngleUnit.DEGREES.toRadians(robot.getHeading());
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
 
-      double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-      double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-      rotX = rotX * 1.1;  // Counteract imperfect strafing
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
 
-      double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-      double frontLeftPower = (rotY + rotX + rx) / denominator;
-      double backLeftPower = (rotY - rotX + rx) / denominator;
-      double frontRightPower = (rotY - rotX - rx) / denominator;
-      double backRightPower = (rotY + rotX - rx) / denominator;
+            // Slow mode
+            if (gamepad1.right_bumper) {
+                frontLeftPower *= 0.4;
+                backLeftPower *= 0.4;
+                frontRightPower *= 0.4;
+                backRightPower *= 0.4;
+            }
 
-      // Slow mode
-      if (gamepad1.right_bumper) {
-        frontLeftPower *= 0.4;
-        backLeftPower *= 0.4;
-        frontRightPower *= 0.4;
-        backRightPower *= 0.4;
-      }
-
-      robot.setDriveTrainPower(frontRightPower, frontLeftPower, backRightPower, backLeftPower);
+            robot.setDriveTrainPower(frontRightPower, frontLeftPower, backRightPower, backLeftPower);
 
       /*
       robot.fl.setPower(frontLeftPower);
@@ -77,98 +77,95 @@ public class Teleop extends LinearOpMode {
 
        */
 
-      if (gamepad2.dpad_down) {
-        slideOut = false;
-        timer2 = 0;
-        timerDone2 = false;
-      }
+            if (gamepad2.dpad_down) {
+                slideOut = false;
+                timer2 = 0;
+                timerDone2 = false;
+            }
 
-      if (!timerDone2) {
-        timer2++;
-      }
-      if (timer2 > 35) {
-        timerDone2 = true;
-      }
+            if (!timerDone2) {
+                timer2++;
+            }
+            if (timer2 > 35) {
+                timerDone2 = true;
+            }
 
-      if (timerDone2 && !slideOut) {
-        horizontalPos = Robot.HORIZONTAL_SLIDE_IN;
-      }
+            if (timerDone2 && !slideOut) {
+                horizontalPos = Robot.HORIZONTAL_SLIDE_TRANSFER;
+            }
 
-      // TODO: direct control horizontal slides (increment/decrement servo position)
-      if (gamepad2.dpad_up) {
-        if (robot.slideOUT.getPosition() != Robot.HORIZONTAL_SLIDE_OUT) {
-          timer = 0;
+            // TODO: direct control horizontal slides (increment/decrement servo position)
+            if (gamepad2.dpad_up) {
+                if (robot.slideOUT.getPosition() != Robot.HORIZONTAL_SLIDE_OUT) {
+                    timer = 0;
+                }
+                horizontalPos = Robot.HORIZONTAL_SLIDE_OUT;
+                timerDone = false;
+                slideOut = true;
+            }
+
+            // Got moved down
+            if (gamepad2.right_stick_y != 0) {
+                slideGoOut = true;
+                horizontalPos = Robot.HORIZONTAL_SLIDE_OUT / 1.2;
+            } else {
+                slideGoOut = false;
+            }
+
+            robot.setHorizontalSlidePos(horizontalPos);
+
+            if (!timerDone) {
+                timer++;
+            }
+            if (timer > 35) {
+                timerDone = true;
+            }
+
+            if (slideOut && timerDone) {
+                if (gamepad2.a) {
+                    robot.rotateIntakeOut();
+                } else {
+                    robot.rotateIntakeFlat();
+                }
+            } else {
+                robot.rotateIntakeUp();
+            }
+
+            robot.setVerticalSlidePower(-gamepad2.right_stick_y);
+
+            outtaking = gamepad1.b;
+
+            if (outtaking && (vSlideLPos + vSlideRPos) / 2 > 10) {
+                robot.outtakeOut();
+            } else {
+                robot.outtakeIn();
+            }
+
+            if (gamepad2.right_bumper) {
+                robot.intake.setPower(1);
+            } else if (gamepad2.left_bumper) {
+                robot.intake.setPower(-1);
+            } else {
+                robot.intake.setPower(0);
+            }
+
+            if (gamepad1.dpad_left) {
+                robot.hang.setPower(1);
+            } else if (gamepad1.dpad_right) {
+                robot.hang.setPower(-1);
+            } else {
+                robot.hang.setPower(0);
+            }
+
+            telemetry.addData("INTAKE ROTATE POS", robot.flipper.getPosition());
+            telemetry.addData("INTAKE POW", robot.intake.getPower());
+            telemetry.addData("OUTTAKE POS", robot.outtake.getPosition());
+            telemetry.addData("H SLIDE POS", robot.slideOUT.getPosition());
+            telemetry.addData("V SLIDE L ENC", vSlideLPos);
+            telemetry.addData("V SLIDE R ENC", vSlideRPos);
+            telemetry.addData("HANG ENC", hangPos);
+            telemetry.addData("Slide go out?: ", slideGoOut);
+            telemetry.update();
         }
-        horizontalPos = Robot.HORIZONTAL_SLIDE_OUT;
-        timerDone = false;
-        slideOut = true;
-      }
-
-      // Got moved down
-      if (gamepad2.right_stick_y != 0) {
-        slideGoOut = true;
-        horizontalPos = Robot.HORIZONTAL_SLIDE_OUT / 1.5;
-      } else {
-        slideGoOut = false;
-      }
-
-      robot.setHorizontalSlidePos(horizontalPos);
-
-      if (!timerDone) {
-        timer++;
-      }
-      if (timer > 35) {
-        timerDone = true;
-      }
-
-      if (slideOut && timerDone) {
-        if (gamepad2.a) {
-          robot.rotateIntakeOut();
-        } else {
-          robot.rotateIntakeFlat();
-        }
-      } else {
-        robot.rotateIntakeUp();
-      }
-
-      robot.setVerticalSlidePower(-gamepad2.right_stick_y);
-
-      outtaking = gamepad1.b;
-
-      if (outtaking && (vSlideLPos + vSlideRPos) / 2 > 10) {
-        robot.outtakeOut();
-      } else {
-        robot.outtakeIn();
-      }
-
-      if (gamepad2.right_bumper) {
-        robot.intake.setPower(1);
-      } else if (gamepad2.left_bumper) {
-        robot.intake.setPower(-1);
-      } else {
-        robot.intake.setPower(0);
-      }
-
-      // TODO: improve (one-button up, one-button down)
-      if (gamepad1.left_trigger > 0.5 && gamepad1.right_trigger > 0.5) {
-        if (gamepad1.dpad_left) {
-          robot.hang.setPower(1);
-        } else if (gamepad1.dpad_right) {
-          robot.hang.setPower(-1);
-        } else {
-          robot.hang.setPower(0);
-        }
-      }
-
-      telemetry.addData("INTAKE ROTATE POS", robot.flipper.getPosition());
-      telemetry.addData("INTAKE POW", robot.intake.getPower());
-      telemetry.addData("OUTTAKE POS", robot.outtake.getPosition());
-      telemetry.addData("H SLIDE POS", robot.slideOUT.getPosition());
-      telemetry.addData("V SLIDE L ENC", vSlideLPos);
-      telemetry.addData("V SLIDE R ENC", vSlideRPos);
-      telemetry.addData("HANG ENC", hangPos);
-      telemetry.addData("Slide go out?: ", slideGoOut);
-      telemetry.update();
     }
-  }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/roadrunner/OneCycleRoadrunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/roadrunner/OneCycleRoadrunner.java
@@ -17,90 +17,89 @@ import org.firstinspires.ftc.teamcode.odom.MecanumDrive;
 @Autonomous(name = "ONE CYCLE ROADRUNNER", group = "ROADRUNNER")
 public class OneCycleRoadrunner extends LinearOpMode {
 
-  public static double blockPickUpX = -49.5;
+    public static double blockPickUpX = -49.5;
 
-  public static double leftPickUpY = -53;
-
-
-  public static double turnBlockAngleDeg = 90;
-  public static double endTurnBlockAngleDeg = 45;
-
-  public static Vector2d bucketVec = new Vector2d (-56, -56);
-
-  public static Vector2d park1Vec = new Vector2d(-48, -36);
-  public static Vector2d park2Vec;
-  public static Vector2d park3Vec;
-
-  //public static VelConstraint slowVel = new TranslationalVelConstraint(40);
-  //public static AccelConstraint slowAccel = new ProfileAccelConstraint(-40, 40);
-
-  private Robot robot;
-  MecanumDrive drive;
-
-  @Override
-  public void runOpMode() throws InterruptedException {
-
-    Pose2d START = new Pose2d(-30.5, -62, Math.toRadians(0));
-
-    Pose2d bucket = new Pose2d(bucketVec, Math.toRadians(45));
-
-    Pose2d rightBlock = new Pose2d(new Vector2d(blockPickUpX, leftPickUpY), Math.toRadians(90));
-
-    Pose2d park1 = new Pose2d(park1Vec, Math.toRadians(180));
-    Vector2d park2Vec = new Vector2d(35, -36);
-    Vector2d park3Vec = new Vector2d(48, -62);
-
-    // INIT
-    telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
-
-    robot = new Robot(this);
-    drive = robot.drive;
-    drive.pose = START;
-
-    robot.initAuton();
-    waitForStart();
-    // START
-
-    //bucket
-    Actions.runBlocking(
-        drive.actionBuilder(drive.pose)
-            .setTangent(90)
-            .splineToLinearHeading(bucket, Math.toRadians(225))
-            .build()
-    );
-
-    robot.deposit();
-
-    //right block
-
-    Actions.runBlocking(
-        drive.actionBuilder(drive.pose)
-            .splineToLinearHeading(rightBlock, Math.toRadians(225))
-            .build()
-    );
+    public static double leftPickUpY = -53;
 
 
+    public static double turnBlockAngleDeg = 90;
+    public static double endTurnBlockAngleDeg = 45;
 
-    pickUpVertical();
+    public static Vector2d bucketVec = new Vector2d(-56, -56);
 
-    robot.waitTime(1000);
+    public static Vector2d park1Vec = new Vector2d(-48, -36);
+    public static Vector2d park2Vec;
+    public static Vector2d park3Vec;
+
+    //public static VelConstraint slowVel = new TranslationalVelConstraint(40);
+    //public static AccelConstraint slowAccel = new ProfileAccelConstraint(-40, 40);
+
+    private Robot robot;
+    MecanumDrive drive;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d START = new Pose2d(-30.5, -62, Math.toRadians(0));
+
+        Pose2d bucket = new Pose2d(bucketVec, Math.toRadians(45));
+
+        Pose2d rightBlock = new Pose2d(new Vector2d(blockPickUpX, leftPickUpY), Math.toRadians(90));
+
+        Pose2d park1 = new Pose2d(park1Vec, Math.toRadians(180));
+        Vector2d park2Vec = new Vector2d(35, -36);
+        Vector2d park3Vec = new Vector2d(48, -62);
+
+        // INIT
+        telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
+
+        robot = new Robot(this);
+        drive = robot.drive;
+        drive.pose = START;
+
+        robot.initAuton();
+        waitForStart();
+        // START
+
+        //bucket
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .setTangent(90)
+                        .splineToLinearHeading(bucket, Math.toRadians(225))
+                        .build()
+        );
+
+        robot.deposit();
+
+        //right block
+
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .splineToLinearHeading(rightBlock, Math.toRadians(225))
+                        .build()
+        );
 
 
-    //bucket
-    Actions.runBlocking(
-        drive.actionBuilder(drive.pose)
-            .strafeTo(new Vector2d(-56, -56))
-            .build()
-    );
+        pickUpVertical();
 
-    robot.waitTime(1000);
-
-    robot.deposit();
-
-    robot.waitTime(1000);
+        robot.waitTime(1000);
 
 
-    //TODO: CHECK IF PARKING WORKS
+        //bucket
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .strafeTo(new Vector2d(-56, -56))
+                        .build()
+        );
+
+        robot.waitTime(1000);
+
+        robot.deposit();
+
+        robot.waitTime(1000);
+
+
+        //TODO: CHECK IF PARKING WORKS
     /*
 
     //park
@@ -118,58 +117,58 @@ public class OneCycleRoadrunner extends LinearOpMode {
 
 
      */
-  }
+    }
 
-  public void pickUpVertical() {
+    public void pickUpVertical() {
 
-    //turn right
-    Actions.runBlocking(
-        drive.actionBuilder(drive.pose)
-            .turnTo(Math.toRadians(turnBlockAngleDeg))
-            .build()
-    );
+        //turn right
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .turnTo(Math.toRadians(turnBlockAngleDeg))
+                        .build()
+        );
 
-    //slide out
-    robot.setHorizontalSlidePos(Robot.HORIZONTAL_SLIDE_OUT);
+        //slide out
+        robot.setHorizontalSlidePos(Robot.HORIZONTAL_SLIDE_OUT);
 
-    robot.waitTime(1000);
+        robot.waitTime(1000);
 
-    //intake down
-    robot.rotateIntakeOut();
+        //intake down
+        robot.rotateIntakeOut();
 
-    robot.waitTime(1000);
+        robot.waitTime(1000);
 
-    //intake on
-    robot.intake.setPower(1);
-    robot.waitTime(500);
+        //intake on
+        robot.intake.setPower(1);
+        robot.waitTime(500);
 
-    robot.waitTime(1000);
+        robot.waitTime(1000);
 
-    //turn left
-    Actions.runBlocking(
-        drive.actionBuilder(drive.pose)
-            .turnTo(Math.toRadians(endTurnBlockAngleDeg))
-            .build()
-    );
+        //turn left
+        Actions.runBlocking(
+                drive.actionBuilder(drive.pose)
+                        .turnTo(Math.toRadians(endTurnBlockAngleDeg))
+                        .build()
+        );
 
-    //wait
-    robot.waitTime(1000);
+        //wait
+        robot.waitTime(1000);
 
-    //intake off
-    robot.intake.setPower(0);
+        //intake off
+        robot.intake.setPower(0);
 
-    //intake up
-    robot.rotateIntakeUp();
+        //intake up
+        robot.rotateIntakeUp();
 
-    robot.waitTime(500);
+        robot.waitTime(500);
 
-    //slide in
-    robot.setHorizontalSlidePos(Robot.HORIZONTAL_SLIDE_IN);
+        //slide in
+        robot.setHorizontalSlidePos(Robot.HORIZONTAL_SLIDE_TRANSFER);
 
-    robot.waitTime(500);
+        robot.waitTime(500);
 
 
-  }
+    }
 
 }
 //mayo
