e38af09465a2c0cd45f44b3010cc8638969ca3e8
andrewaburustum <aaburustum26@saddleriverday.org>
2025-03-02 12:35:48 -0500

    thing

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -6,87 +6,20 @@ import com.pedropathing.localization.constants.ThreeWheelConstants;
 public class LConstants {
 
   static {
-    /*
-    PinpointConstants.forwardY  = 3.5;
-    PinpointConstants.strafeX = 0;
 
-    PinpointConstants.distanceUnit = DistanceUnit.INCH;
-    PinpointConstants.hardwareMapName = "pinpoint";  // change name
-
-
-    PinpointConstants.useYawScalar = false;
-    PinpointConstants.yawScalar = 1.0;
-
-    PinpointConstants.useCustomEncoderResolution = false;
-    PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
-    PinpointConstants.customEncoderResolution = 13.26291192; // Useless ?
-
-    PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
-    PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
-
-     */
-
-    //TODO: Tune these
-    /*
-        a) Forward Localizer Tuner
-        Position a ruler alongside your robot.
-
-        Push the robot forward by the desired distance (default is 48 inches).
-
-        The tuner will display two numbers:
-
-        First number: Distance the robot thinks it has traveled.
-
-        Second number (multiplier)
-
-        (Optional) Run multiple tests and average the multipliers for better accuracy.
-
-        Input this value in LConstants as ThreeWheelConstants.forwardTicksToInches = [multiplier], where [multiplier] is the value you obtained from the tuner.
-
-        b) Lateral Localizer Tuner
-        Position a ruler alongside your robot.
-
-        Push the robot sideways (strafing) by the desired distance (default is 48 inches).
-
-        The tuner will display two numbers:
-
-        First number: Distance the robot thinks it has traveled laterally.
-
-        Second number (multiplier)
-
-        (Optional) Run multiple tests and average the multipliers for better accuracy.
-
-        Input this value in LConstants as ThreeWheelConstants.strafeTicksToInches = [multiplier], where [multiplier] is the value you obtained from the tuner.
-
-        c) Turn Localizer Tuner
-        Position your robot facing a recognizable landmark, like a field tile edge.
-
-        Spin the robot counterclockwise for one full rotation (or your desired angle).
-
-        The tuner will display two numbers:
-
-        First number: Distance the robot thinks it has spun.
-
-        Second number (multiplier)
-
-        (Optional) Run multiple tests and average the multipliers for better accuracy.
-
-        Input this value in LConstants as ThreeWheelConstants.turnTicksToInches = [multiplier], where [multiplier] is the value you obtained from the tuner.
-
-     */
     ThreeWheelConstants.forwardTicksToInches = .001989436789;
     ThreeWheelConstants.strafeTicksToInches = .001989436789;
     ThreeWheelConstants.turnTicksToInches = .001989436789;
 
     //TODO: Measure the pods (Robot Grid: https://pedropathing.com/localization/setup.html#robot-coordinate-grid)
-    ThreeWheelConstants.leftY = 1;
-    ThreeWheelConstants.rightY = -1;
-    ThreeWheelConstants.strafeX = -2.5;
+    ThreeWheelConstants.leftY = 7.5;
+    ThreeWheelConstants.rightY = -7.5;
+    ThreeWheelConstants.strafeX = 2.75;
 
     //TODO: Check the names
-    ThreeWheelConstants.leftEncoder_HardwareMapName = "bl";
-    ThreeWheelConstants.rightEncoder_HardwareMapName = "fl";
-    ThreeWheelConstants.strafeEncoder_HardwareMapName = "fr";
+    ThreeWheelConstants.leftEncoder_HardwareMapName = "fr";
+    ThreeWheelConstants.rightEncoder_HardwareMapName = "bl";
+    ThreeWheelConstants.strafeEncoder_HardwareMapName = "br";
 
     //TODO:Check these
     /*
